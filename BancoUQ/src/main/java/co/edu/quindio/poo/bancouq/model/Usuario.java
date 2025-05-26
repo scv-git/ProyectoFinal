689567120c650883cb6923bc3010d07c2f00739e
Arcilaaa <Santiago.arcilas@uqvirtual.edu.co>
2025-05-26 00:44:02 -0500

    a
    a

--- BancoUQ/src/main/java/co/edu/quindio/poo/bancouq/model/Usuario.java
+++ BancoUQ/src/main/java/co/edu/quindio/poo/bancouq/model/Usuario.java
@@ -1,39 +1,81 @@
-package main.java.co.edu.quindio.poo.bancouq.model;
+package co.edu.quindio.poo.bancouq.model;
 
 public abstract class Usuario {
-}
-public Usuario buscarusuario(String identifiacion){
-    if (listausuarios == null)
-        return null;
-    for(Usuario usuario : listausuarios){
-        if(usuario.getIdentificacion().equals(identifiacion)){
-            return usuario;
-        }
-    }
-    return null;
-}
-public void registrarusuario (Usuario usuario) {
-    listausuarios.add(usuario);
-}
-public void eliminarusuario(Usuario usuario) {
-    listausuarios.remove(usuario);
-}
-public String modificarUsuario(String identificacion, String nuevoNombre, String nuevoApellido,
-                               String nuevoCorreo, String nuevoTelefono, String nuevoPin) {
-    Usuario usuario = buscarusuario(identificacion);
+    private String nombres;
+    private String apellidos;
+    private String identificacion;
+    private String correo;
+    private String pin;
+    private String telefono;
 
-    if (usuario == null) {
-        return "Usuario no encontrado";
+    public Usuario(String nombres, String apellidos, String correo, String pin, String telefono,String identificacion) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.pin = pin;
+        this.telefono = telefono;
+        this.identificacion = identificacion;
     }
 
-    usuario.setNombres(nuevoNombre);
-    usuario.setApellidos(nuevoApellido);
-    usuario.setCorreo(nuevoCorreo);
-    usuario.setTelefono(nuevoTelefono);
-    usuario.setPin(nuevoPin);
+    public String getNombres() {
+        return nombres;
+    }
 
-    return "Usuario modificado exitosamente.";
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", correo='" + correo + '\'' +
+                ", pin='" + pin + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+
+
 }
-public ArrayList<Usuario> listarusuarios() {
-    return listausuarios;
-}
+
